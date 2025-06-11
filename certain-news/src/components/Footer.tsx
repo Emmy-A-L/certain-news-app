99d2edc0ca07f2c532fa73664fe0cc16ef63e96f
Emmy-A-L <elot833@gmail.com>
2025-06-12 00:44:53 +0100

    fix: minor modifications

--- certain-news/src/components/Footer.tsx
+++ certain-news/src/components/Footer.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer className="fixed bottom-0 w-full bg-purple-800 text-white py-8 mt-12">
+    <footer className="w-full bg-purple-800 text-white py-8 mt-12">
       <div className="container mx-auto px-4">
         <div className="flex flex-col md:flex-row justify-between items-center">
           <div className="mb-4 md:mb-0 text-center md:text-left">
