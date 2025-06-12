cf9410c06989f4a2697fe0329844923c14daf51f
Emmy-A-L <elot833@gmail.com>
2025-06-12 01:00:46 +0100

    Feat: fixed api data fetching issue.. crazy stuff. lol

--- certain-news/src/services/api.ts
+++ certain-news/src/services/api.ts
@@ -1,6 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API_BASE_URL = import.meta.env.REACT_APP_API_BASE_URL || 'http://localhost:4040/api';
+const API_BASE_URL = import.meta.env.VITE_API_URL;
+console.log(`API: ${API_BASE_URL}`);
 
 const api = axios.create({
   baseURL: API_BASE_URL,
