ac9e8b86723baea4d4cb69e63cfbb8abdc639b9e
Emmy-A-L <elot833@gmail.com>
2025-06-13 01:41:01 +0100

    fix: comment out

--- certain-news/src/services/api.ts
+++ certain-news/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_BASE_URL = import.meta.env.VITE_API_URL;
-console.log(`API: ${API_BASE_URL}`);
+// console.log(`API: ${API_BASE_URL}`);
 
 const api = axios.create({
   baseURL: API_BASE_URL,
